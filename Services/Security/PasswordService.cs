ac9ba95f6f992379be98a357a4b8e00a406d98ed
Fabian Martin <fabianmartinanz@gmail.com>
2023-11-18 10:21:42 -0500

    Feat: Added passwords helpers
    Using BCrypt package for hash and validate

---
+++ Services/Security/PasswordService.cs
@@ -0,0 +1,15 @@
+namespace skyhub.Services.Security
+{
+    public class PasswordService
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool ValidatePassword(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
